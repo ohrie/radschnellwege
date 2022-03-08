f6350821ba1006ccdb644bd9f675ab817062f4d4
Henri <henri@fixmycity.de>
2022-03-08 11:24:12 +0100

    Convert to complete sepcification

--- scripts/convert-csv-metajson.py
+++ scripts/convert-csv-metajson.py
@@ -4,23 +4,11 @@ import csv
 import json
 import time
 
-column_mapping = {
-    "ID": "id",
-    "Abk\u00fcrzung": "ref",
-    "Titel": "name",
-    "von": "from",
-    "bis": "to",
-    "(Kurzbeschreibung)": "description",
-    "Projektstand": "status",
-    "Kosten": "cost",
-    "Fertigstellung": "finished",
-    "Auftraggeber": "authority",
-}
-
 column_removals = [
     "", "ID_qgis", "Partnerkommune", "Projektwebsite", "Beteiligungsverfahren",
     "Wiki-Update", "Quellen", "Bundesland", "Kontakt", "Linestring",
-    "Trassenf\u00fchrung", "Foto", "L\u00e4nge"
+    "Trassenf\u00fchrung", "Foto", "L\u00e4nge", "Auftraggeber", "von",
+    "bis", "Abk\u00fcrzung", "Titel", "(Kurzbeschreibung)"
 ]
 
 def csv_to_json(csvFilePath, jsonFilePath):
@@ -33,12 +21,32 @@ def csv_to_json(csvFilePath, jsonFilePath):
 
         #convert each csv row into python dict
         for row in list(csvReader):
-            rowCopy = row
+            rowCopy = {}
             for key in list(row):
-                if key in column_removals:
-                    del rowCopy[key] 
-                if key in column_mapping:
-                    rowCopy[column_mapping[key]] = rowCopy.pop(key)
+                rowCopy["id"] = row["ID"]
+                rowCopy["cost"] = row["Kosten"]
+                rowCopy["finished"] = row["Fertigstellung"]
+                rowCopy["state"] = row["Projektstand"]
+                rowCopy["planning_phase"] = ""
+                rowCopy["detail_level"] = ""
+                rowCopy["general"] = {
+                    "ref": row["Abkürzung"],
+                    "name": row["Titel"],
+                    "from": row["von"],
+                    "to": row["bis"],
+                    "description": row["(Kurzbeschreibung)"],
+                    "slug": ""
+                }
+                rowCopy["stakeholders"] = [{
+                    "name": row["Auftraggeber"],
+                    "roles": ["authority"],
+                    "description": ""
+                }]
+                rowCopy["references"] = {
+                    "website": "",
+                    "osm_relation": ""
+                }
+                
             jsonArray.append(rowCopy)
   
     #convert python jsonArray to JSON String and write to file
